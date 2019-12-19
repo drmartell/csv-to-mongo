fda0b018fb882b4dcfcb012e197bb17295c5bbd7
Casey Martell <drmartell@gmail.com>
2019-12-19 14:09:27 -0800

    aggregations with tests

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
-
 const PORT = process.env.PORT || 7890;
-require('./lib/app').app.listen(PORT, () =>
+require('./lib/app').listen(PORT, () =>
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`));
